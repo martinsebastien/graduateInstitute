cf7096ec34c6a4eabc7443201e48253956772bbb
SMartin <sebmartinus@gmail.com>
2017-02-25 17:32:08 +0100

    Add background component

---
+++ src/components/Background/Background.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Style from 'Background.scss';
+
+export default class Background extends Component {
+
+  state = {};
+
+  render() {
+    return (
+      <p>Hello World</p>
+    );
+  }
+
+}
