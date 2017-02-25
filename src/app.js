0f08da563c0d13a433898baa89f9baef22556908
Nolan Rigo <nolan.rigo@gmail.com>
2017-02-25 16:15:10 +0100

    Add eslint

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
