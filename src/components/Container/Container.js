97648e295b5eb8e929254137c76c1aebb10c18d5
Nolan Rigo <nolan.rigo@gmail.com>
2017-02-26 17:38:46 +0100

    Add padding on <Container />

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -5,13 +5,19 @@ import style from './container.scss';
 export default class Container extends Component {
 
   static propTypes = {
-    children: PropTypes.element.isRequired,
+    children: PropTypes.node.isRequired,
   };
 
   state = {};
 
   render() {
-    return <div className={style.container}>{this.props.children}</div>;
+    return (
+      <div className={style.container}>
+        <div className={style.inner} {...this.props}>
+          {this.props.children}
+        </div>
+      </div>
+    );
   }
 
 }
