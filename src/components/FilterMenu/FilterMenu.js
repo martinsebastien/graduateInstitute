4a656a4e31d37f052bdc7a9b07916af9db180232
Nolan Rigo <nolan.rigo@gmail.com>
2017-02-26 17:41:01 +0100

    Create <Notify />

---
+++ src/components/FilterMenu/FilterMenu.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import style from './filter-menu.scss';
+
+export default class FilterMenu extends Component {
+
+  state = {};
+
+  componentWillMount() {
+    this.selectId = _.uniqueId();
+  }
+
+  selectId = '';
+  idFor(filterName) {
+    return `${filterName}-${this.selectId}`;
+  }
+
+  selectId = '';
+
+  render() {
+    return (
+      <div className={style.filterMenu}>
+        <ul>
+          <li>
+            <input type="checkbox" name="filter[bachelors]" id={this.idFor('bachelors')} />
+            <label htmlFor={this.idFor('bachelors')}>Bachelors</label>
+          </li>
+          <li>
+            <input type="checkbox" name="filter[masters]" id={this.idFor('masters')} />
+            <label htmlFor={this.idFor('masters')}>Masters</label>
+          </li>
+          <li>
+            <input type="checkbox" name="filter[phd]" id={this.idFor('phd')} />
+            <label htmlFor={this.idFor('phd')}>Phd</label>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+}
