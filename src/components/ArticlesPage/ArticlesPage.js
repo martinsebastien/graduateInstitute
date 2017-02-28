ab558aa45446c7d74b416791664e189e5f8fadd7
Nolan Rigo <nolan.rigo@gmail.com>
2017-02-28 17:17:48 +0100

    Create <SectionTitle />

--- src/components/ArticlesPage/ArticlesPage.js
+++ src/components/ArticlesPage/ArticlesPage.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import Container from '../Container';
+import SectionTitle from '../SectionTitle';
+
 export default class ArticlesPage extends Component {
 
   state = {};
 
   render() {
-    return <p>ArticlesPage</p>;
+    return (
+      <div className={style.articles}>
+        <Container>
+          <SectionTitle>Pinned Articles</SectionTitle>
+          <SectionTitle>All Articles</SectionTitle>
+        </Container>
+      </div>
+    );
   }
 
 }
