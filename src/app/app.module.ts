df6c5c63855613964bb964a94961ddd98755efbf
Nolan Rigo <nolan.rigo@gmail.com>
2017-04-19 18:38:39 +0200

    Create HttpService http angular library wrapper for request to graduateinstitute backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
+import { HttpService } from '../services/http.service';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -22,9 +24,17 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    {
+      provide: ErrorHandler,
+      useClass: IonicErrorHandler,
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-CH',
+    },
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    HttpService,
   ]
 })
 export class AppModule {}
---
+++ src/services/http.service.ts
@@ -0,0 +1,54 @@
+import { Http, RequestOptions, RequestOptionsArgs, Response, RequestMethod, Request } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// Widely inspirated by https://gist.github.com/chandermani/9166abe6e6608a31f471
+
+export { Response, RequestOptionsArgs };
+
+@Injectable()
+export class HttpService {
+
+  private base = 'http://graduateinstitute.blackpen.tv/wp-json/wp/v2';
+
+  constructor(
+    private http: Http,
+  ) {}
+
+  public get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.request(RequestMethod.Get, url, null, options);
+  }
+
+  public post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.request(RequestMethod.Post, url, body, options);
+  }
+
+  public put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.request(RequestMethod.Put, url, body, options);
+  }
+
+  public delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.request(RequestMethod.Delete, url, null, options);
+  }
+
+  public patch(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.request(RequestMethod.Patch, url, body, options);
+  }
+
+  public head(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.request(RequestMethod.Head, url, null, options);
+  }
+
+  private request(method: RequestMethod, url: string, body?: string, options?: RequestOptionsArgs): Observable<Response> {
+    let requestOptions = new RequestOptions(Object.assign({ method, body, url: this.buildUrl(url) }, options));
+
+    return this.http.request(new Request(requestOptions));
+
+    // TODO: catch some request failures like "no internet connection"
+  }
+
+  private buildUrl(endpoint: string): string {
+    return this.base + endpoint;
+  }
+
+}
