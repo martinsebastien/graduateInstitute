55c2914ae1ee6d35607c46ecf3c045d7b829b87d
Nolan Rigo <nolan.rigo@gmail.com>
2017-05-06 18:19:08 +0200

    :(

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'gi-header',
   templateUrl: 'header.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(
+    public navCtrl: NavController,
+  ) {}
+
+  backToHome() {
+    this.navCtrl.popToRoot();
+  }
+
+}
