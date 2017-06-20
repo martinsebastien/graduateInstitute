f81298d86bb3a15cb04fe43bba1dc62e12469ae2
Nolan Rigo <nolan.rigo@gmail.com>
2017-06-20 15:35:29 +0200

    Create <gi-video /> component

---
+++ src/components/video/video.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import getYoutubeId from 'get-youtube-id';
+
+@Component({
+  selector: 'gi-video',
+  templateUrl: 'video.html'
+})
+export class VideoComponent {
+
+  public videoUrl: SafeResourceUrl;
+
+  constructor(
+    public domSanitizer: DomSanitizer,
+  ) {}
+
+  @Input()
+  public set url(val: string) {
+    const id = getYoutubeId(val);
+    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${id}`);
+  }
+
+}
