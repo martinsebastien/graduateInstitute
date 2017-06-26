268df6414c194b2a3d10378c2a5041fd625fc656
Sébastien Martin <sm.webdevelop@gmail.com>
2017-06-26 20:37:08 +0200

    correct web file

--- src/models/web.ts
+++ src/models/web.ts
@@ -10,6 +10,7 @@ export class Web {
     const t = new Web;
     t.src = link_web;
     t.title = link_web.substr(link_web.lastIndexOf('/') + 1, (link_web.lastIndexOf('.') - link_web.lastIndexOf('/')) - 1);
+    t.title == '' && link_web;
     return t;
 
   }
